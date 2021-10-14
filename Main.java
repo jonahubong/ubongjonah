c6504c244be7e99c5de60dbf18c274bb1524b65e
jonahubong <92110565+jonahubong@users.noreply.github.com>
2021-10-14 09:41:32 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,14 +3,7 @@ package jonah;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        Main main = new Main();
-        main.Hello();
-    }
 
-    public void Hello() {
-        System.out.println();
-        System.out.println("Hello, my name is Akan");
-        System.out.println("Hello, your team members name, my name is Ubong.");
+        System.out.print("Hello, my name is Ubong");
     }
 }
